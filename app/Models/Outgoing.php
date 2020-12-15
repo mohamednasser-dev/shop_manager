9aff05ca07288ee887782dd8eeb73c00f45ea66a
MostafaElebzary <mostafaelebzary@gmail.com>
2020-12-15 22:40:30 +0200

    bezra

--- app/Models/Outgoing.php
+++ app/Models/Outgoing.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Outgoing extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'cost', 'date', 'user_id'
+    ];
+
+    public function employee()
+    {
+        return $this->hasOne('App\Models\User', 'id', 'user_id');
+    }
 }
